d42f0128cfdf6b0b13db556533979c61e379b2d6
Mingx1994x <mggxmingx@gmail.com>
2024-11-25 21:23:20 +0800

    refactor: 將alert相關程式法拆分成alert.js feat:調整confirm alert視窗函式參數，新增後台alert視窗等功能

---
+++ assets/js/alert.js
@@ -0,0 +1,63 @@
+import Swal from 'sweetalert2';
+
+export { toastAlert, confirmDialog, successDialog, loadingAlert };
+
+const toastAlert = () => {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: "top-end",
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+        }
+    });
+    Toast.fire({
+        icon: "success",
+        title: "成功加入購物車"
+    });
+}
+
+const loadingAlert = (title) => {
+    Swal.fire({
+        title,
+        timer: 800,
+        timerProgressBar: true,
+        allowOutsideClick: false,
+        didOpen: () => {
+            Swal.showLoading();
+        },
+    });
+}
+
+const confirmDialog = (title, fn, text) => {
+    Swal.fire({
+        title,
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "移除",
+        cancelButtonText: "取消"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            fn();
+            Swal.fire({
+                text,
+                icon: "success"
+            });
+        }
+    });
+}
+
+const successDialog = () => {
+    Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "訂單已送出！",
+        showConfirmButton: false,
+        timer: 1500
+    });
+}
--- main.js
+++ main.js
@@ -2,4 +2,5 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 import './assets/js/menu';
+import './assets/js/alert';
 // console.log("Hello world!");
