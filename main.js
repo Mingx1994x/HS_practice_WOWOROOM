d15397f66207f638ed1a43ec5f2915cd3bb053ae
Mingx1994x <mggxmingx@gmail.com>
2024-11-26 00:27:38 +0800

    refactor: 優化精簡新增購物車資料邏輯

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 import './assets/js/menu';
-import './assets/js/alert';
+// import './assets/js/alert';
 // console.log("Hello world!");
